64377218b0f5cc5388bf72801e2d0ef2f7628653
Saravanan Ramanathan <srvnnramanathan@gmail.com>
2022-05-27 17:12:27 +0530

    Dashboard
    made a user for dashboard
    linked dashboard with models
    overall got the dashboard working

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from main import models
 # Register your models here.
+#linking dashboard with the database part
+
+admin.site.register(models.ToDoList)
+admin.site.register(models.Item)
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -15,8 +15,15 @@ def index(response):
 def id(response,idValue:int):
     print("testing id")
     t=models.ToDoList;
-    item=t.objects.get(id=idValue).item_set.get(id=1)
+    items=t.objects.get(id=idValue).item_set.all()
     #print(t.objects.get(id=idValue))
-    return HttpResponse(f"<h1>The value for id {idValue} is:</h1>\
-        <h2>{t.objects.get(id=idValue).name}</h2>\
-                 <p>{item.text}={item.complete}<p>")
+    text1=f"<h1>The value for id {idValue} is:</h1>\
+        <h2>{t.objects.get(id=idValue).name}</h2>"
+    text2=""
+    if(not items.exists()):
+        text2="<p>no items found.<p>"
+    print(f"checking stuff: {items}")
+    
+    for i in items:
+        text2+=f"<p>{i.text}={i.complete}<p>"
+    return HttpResponse(text1+text2)
