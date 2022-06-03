cd611c56ce3d2b546d249bb734b9b794baee8d12
Saravanan Ramanathan <srvnnramanathan@gmail.com>
2022-06-03 12:59:04 +0530

    testing sub rows in tables

--- mysite/main/static/main/display_table_click.js
+++ mysite/main/static/main/display_table_click.js
@@ -1,5 +1,8 @@
 
     $('#display-table > tbody > tr').click(function() {
         //alert("you are being redirected to id: "+$(this).data("display-table-id"))
-        window.location =("/id/"+$(this).data("display-table-id"));
+        //window.location =("/id/"+$(this).data("display-table-id"));
+        //alert("testing id : "+$(this).data("display-table-id"));
+        $('.dataid'+$(this).data("display-table-id")).collapse('toggle');
+        
     });
