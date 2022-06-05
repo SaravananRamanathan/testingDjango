3cc4e5b7cac0742a2d7b8d805542ef96ead97b68
Saravanan Ramanathan <srvnnramanathan@gmail.com>
2022-06-05 17:38:49 +0530

    delete old database
    made new database with users as foreign key to todolist

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -3,11 +3,11 @@ database modles
 modling info-to make it easy to grab info
 """
 from django.db import models
-from django.forms import CharField
-
+from django.contrib.auth.models import User
 # Create your models here.
 class ToDoList(models.Model):
     "database object called ToDoList"
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     name=models.CharField(max_length=200)
 
     def __str__(self):
